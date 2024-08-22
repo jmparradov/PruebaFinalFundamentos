10140ad39bab3722e647f78ff3923749db437ecf
Bikings <jmparradov@gmail.com>
2024-08-22 16:29:45 -0500

    csv change to com

---
+++ final.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 16:27:27 2024
+
+@author: jmpar
+"""
+
+import pandas as pd
+
+df_robery  = pd.read_csv('https://raw.githubusercontent.com/jmparradov/PruebaFinalFundamentos/main/robbery%20of%20people%20in%20Medellin.csv')
